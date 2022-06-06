7ff29ab780fba7cad42e54fca4c8aae77fc011d0
AreyBRSK <areybrsk@gmail.com>
2022-06-06 14:27:23 +0300

    Traffic light View Traffic light switching logic

--- TrafficLight_SwiftUI/ContentView.swift
+++ TrafficLight_SwiftUI/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+enum TrafficLight {
+    case none
+    case red
+    case yellow
+    case green
+}
+
 struct ContentView: View {
+    @State private var currentLight = TrafficLight.none
+    @State private var redLightOpacity = 0.5
+    @State private var yellowLightOpacity = 0.5
+    @State private var greenLightOpacity = 0.5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                Text("Traffic Light")
+                    .font(.title)
+                    .foregroundColor(.white)
+                Spacer()
+                ColorCircleView(color: .red, opacity: redLightOpacity)
+                ColorCircleView(color: .yellow, opacity: yellowLightOpacity)
+                ColorCircleView(color: .green, opacity: greenLightOpacity)
+                Spacer()
+                Button(action: {
+                    switchTrafficLight()
+                }) {
+                    Text(currentLight == TrafficLight.none ? "Start" : "Next")
+                        .font(.title)
+                }
+            }
+            .padding(.top)
+        }
+    }
+    
+    private func switchTrafficLight() {
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            redLightOpacity = 0.5
+            yellowLightOpacity = 1
+        case .yellow:
+            currentLight = .green
+            yellowLightOpacity = 0.5
+            greenLightOpacity = 1
+        case .green, .none:
+            currentLight = .red
+            greenLightOpacity = 0.5
+            redLightOpacity = 1
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewInterfaceOrientation(.landscapeRight)
     }
 }
